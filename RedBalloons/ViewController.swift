10fafa318db117e0995c4e8c2249a9f5830d3979
Dennis Ginley <dennis.ginley@gmail.com>
2014-10-07 20:38:31 -0700

    Added comments to explain what I did

--- RedBalloons/ViewController.swift
+++ RedBalloons/ViewController.swift
@@ -5,6 +5,20 @@
 //  Created by Dennis Ginley on 10/6/14.
 //  Copyright (c) 2014 Dennis Ginley. All rights reserved.
 //
+//  viewDidLoad()
+//  Created a struct to hold a Balloon instance
+//  Created an array to hold 99 Balloon structs
+//  Created an array to hold the four balloonImage name strings
+//  Using a for loop, generated 99 Balloon structs and added them to the Balloon array
+//  for each Balloon struct generated above, used a random number between 0 and 3 to pick   
+//  a balloon image to use with it
+//  
+//  toolbarButtonPressed
+//  Used a random number between 0 and 99 to pick which balloon to display next, while  
+//  using an if statement to make sure that the same image was not displayed twice in
+//  a row.
+
+
 
 import UIKit
 
@@ -34,8 +48,8 @@ class ViewController: UIViewController {
         while isBalloonNameTheSame
         {
             index = Int(arc4random_uniform(UInt32(numberOfBalloons)))
-            println("balloonName = \(balloonArray[index].balloonImage)")
-            println("oldballoonName = \(oldBalloonName)")
+//            println("balloonName = \(balloonArray[index].balloonImage)")
+//            println("oldballoonName = \(oldBalloonName)")
             if balloonArray[index].balloonImage != oldBalloonName
             {
                 isBalloonNameTheSame = false
@@ -52,7 +66,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imageView.image = UIImage(named: "BerlinTVTower.jpg")
-        
         
 
         
